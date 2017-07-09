7d9be5e0985d6703c09a3fd01ff4a2fb8514d7e7
Digi-id <digi.id.co@gmail.com>
2017-07-09 16:00:45 -0700

    add plugin

---
+++ public/javascripts/template.js
@@ -0,0 +1,19 @@
+$(".bg").interactive_bg({
+   strength: 25,
+   scale: 1.05,
+   animationSpeed: "100ms",
+   contain: true,
+   wrapContent: false
+ });
+
+$(document).ready(function(){
+    $(".bg").interactive_bg(); // function call
+});
+ 
+  // change background size on window resize
+  $(window).resize(function() {
+      $(".bg > .ibg-bg").css({
+        width: $(window).outerWidth(),
+        height: $(window).outerHeight()
+      })
+   })
